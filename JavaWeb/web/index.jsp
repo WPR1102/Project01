62696554bacfc8afb366c689c3c365ff20467300
WPR1102 <1158463714@qq.com>
2018-11-29 20:02:16 -0500

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: WPR1102
+  Date: 2018/11/28
+  Time: 15:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>ganjinzhaogongzuo</title>
+  </head>
+  <body>
+    <h1 style="color: orange">Hello IDEA!!!</h1>
+  </body>
+</html>
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.atguigu.java;
+
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        ArrayList arrayList = new ArrayList<>();
+        Date date = new Date();
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public void method(){
+        System.out.println("sss");
+    }
+}
